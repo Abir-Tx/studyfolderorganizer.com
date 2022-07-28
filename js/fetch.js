be7954206b8aedc82214fb0a741e5f233df5a77e
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-07-29 01:43:07 +0600

    [CLEANUP] Remove the debug message
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- js/fetch.js
+++ js/fetch.js
@@ -8,11 +8,9 @@ var latest_github_version;
 /* If there is session storage data then load the data/texts from the session else load them directly 
 from the API and save them to the session manager */
 if (sessionStorage.length > 0) {
-	console.log("Loaded from session storage");
 	latest_github_release_elem.innerHTML = 'Latest Version: ' + sessionStorage.getItem('latest_version').slice(1, (sessionStorage.getItem('latest_version').length - 1));
 }
 else {
-	console.log("you know it");
 	fetch('https://api.github.com/repos/Abir-Tx/StudyFolderOrganizer-GUI/releases')
 		.then(res => res.json())
 		.then(data => {
