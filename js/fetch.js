57a94c3072a308a70fafbb542e4fca6964437c8a
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-07-29 01:40:53 +0600

    Create and make use of `sessionStorage`
    - Added session storage system. So the API response will be saved to the session storage so users will feel faster reload and visit to other pages
    - I have done this as I generally won't update the software that much it's more of a final product now so there is no need of frequent check of the `latest version tag`
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,25 @@
 /* JS file used to fetch github details to the website mainly to fetch the latest release tag using GitHub API */
 
 // get the elem in which the latest tag will be loaded
-var latest_github_release = document.getElementById('latest_github_version');
-console.log(latest_github_release.innerHTML);
+var latest_github_release_elem = document.getElementById('latest_github_version');
+var latest_github_version;
 
-fetch('https://api.github.com/repos/Abir-Tx/StudyFolderOrganizer-GUI/releases')
-	.then(res => res.json())
-	.then(data => {
-		latest_github_release.innerHTML = 'Latest Version: ' + data[0]['tag_name'];
-	})
+
+/* If there is session storage data then load the data/texts from the session else load them directly 
+from the API and save them to the session manager */
+if (sessionStorage.length > 0) {
+	console.log("Loaded from session storage");
+	latest_github_release_elem.innerHTML = 'Latest Version: ' + sessionStorage.getItem('latest_version').slice(1, (sessionStorage.getItem('latest_version').length - 1));
+}
+else {
+	console.log("you know it");
+	fetch('https://api.github.com/repos/Abir-Tx/StudyFolderOrganizer-GUI/releases')
+		.then(res => res.json())
+		.then(data => {
+			latest_github_version = data[0]['tag_name'];
+			latest_github_release_elem.innerHTML = 'Latest Version: ' + latest_github_version;
+
+			// Save to session storage
+			sessionStorage.setItem('latest_version', JSON.stringify(latest_github_version));
+		})
+}
